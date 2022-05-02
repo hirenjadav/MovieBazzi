8c5882b44d40f4be7b3e14c533d4e52b5ccfa33a
Hiren Jadav <h.r.jadav252@gmail.com>
2022-05-02 16:59:39 +0530

    changes in server,js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 8080;
 app.use(express.static(path.join(__dirname, "build")));
 
 // This route serves the React app
-app.get("/", (req, res) =>
+app.get("/*", (req, res) =>
   res.sendFile(path.resolve(__dirname, "build", "index.html"))
 );
 
